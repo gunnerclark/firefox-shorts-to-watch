e5358c041bb977ab464c690831922556244873a4
clark <63522725+gunnerclark@users.noreply.github.com>
2023-06-22 11:45:21 -0500

    - removed console logs - added icon - adjusted manifest

--- background.js
+++ background.js
@@ -20,10 +20,7 @@ function checkShort(tabId, changeInfo, tab) {
     thisUrl = changeInfo.url;
 
     if (thisUrl !== undefined && match(`https://www.youtube.com/shorts/*`, thisUrl)) {
-        console.log(`This is a YouTube Shorts url.`);
         browser.tabs.update(tabId, {url: `https://www.youtube.com/watch/${urlTail}`});
-    } else {
-        console.log(`This is not a YouTube Short OR the code is broken.`)
     }
 }
 
